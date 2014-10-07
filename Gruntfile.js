c76847f3beeadb907fda954cab31d06f1f131f2b
Vít Mádr <vit.madr@gmail.com>
2014-10-07 23:28:08 +0200

    nodeing

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'lib/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    //---
+
+    grunt.registerTask('default', ['jshint']);
+
+};
---
+++ Ideploy.js
@@ -0,0 +1,11 @@
+/**
+ * Ideploy module
+ *
+ * @param options
+ * @constructor
+ */
+'use strict';
+
+var Client = require('./lib/Client.js');
+
+module.exports = Client;
